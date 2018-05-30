e027ba829643d2c79b531287049784116ebcd7de
Chad Kreutzer <kanthalion@gmail.com>
2018-05-30 06:45:57 +0000

    computerPlay written

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Rock Paper Scissors Game
+
+const randomNumber = () => Math.floor(Math.random() * 3);
+
+function computerPlay(randomNumber) {
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[randomNumber];
+}
+
+console.log(computerPlay(randomNumber()));
