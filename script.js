682aa81d00d5ac1e750732298ea5afb93c97d864
Chad Kreutzer <kanthalion@gmail.com>
2018-05-30 11:13:10 +0000

    make prettier

--- script.js
+++ script.js
@@ -3,46 +3,52 @@
 const randomNumber = () => Math.floor(Math.random() * 3);
 
 function computerPlay(randomNumber) {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[randomNumber];
+	const choices = ['rock', 'paper', 'scissors'];
+	return choices[randomNumber];
 }
 
 function playRound(playerSelection, computer = computerPlay(randomNumber())) {
-    const player = String(playerSelection).toLowerCase();
-    if (player === "rock" || player === "paper" || player === "scissors") {
-        if (player === computer) {
-            return "It's a tie, try again!";
-        }
-        switch (player) {
-            case "rock":
-                return (computer === 'scissors') ? "You Win! Rock beats Scissors!" : "You Lose! Paper beats Rock!";
-            case "paper":
-                return (computer === 'rock') ? "You Win! Paper beats Rock!" : "You Lose! Scissors beats Paper!";
-            default:
-                return (computer === 'paper') ? "You Win! Scissors beats Paper!" : "You Lose! Rock beats Scissors!";
-        }
-    }
-    throw new Error("You must chose 'Rock', 'Paper', or 'Scissors'");
+	const player = String(playerSelection).toLowerCase();
+	if (player === 'rock' || player === 'paper' || player === 'scissors') {
+		if (player === computer) {
+			return "It's a tie, try again!";
+		}
+		switch (player) {
+			case 'rock':
+				return computer === 'scissors'
+					? 'You Win! Rock beats Scissors!'
+					: 'You Lose! Paper beats Rock!';
+			case 'paper':
+				return computer === 'rock'
+					? 'You Win! Paper beats Rock!'
+					: 'You Lose! Scissors beats Paper!';
+			default:
+				return computer === 'paper'
+					? 'You Win! Scissors beats Paper!'
+					: 'You Lose! Rock beats Scissors!';
+		}
+	}
+	throw new Error("You must chose 'Rock', 'Paper', or 'Scissors'");
 }
 
 function game(playerChoice) {
-    const score = [0, 0];
-    let roundResult, round = 1;
-    while (round <= 5) {
-        roundResult = playRound(playerChoice());
-        console.log(roundResult);
-        if (roundResult.includes("tie")) {
-            continue;
-        }
-        score[roundResult.includes("Win") ? 0 : 1]++;
-        round++;
-    }
-    if (score[0] > score[1]) {
-        console.log(`You win! ${score[0]} to ${score[1]}!`);
-    }
-    else {
-        console.log(`You lose! ${score[1]} to ${score[0]}!`);
-    }
+	const score = [0, 0];
+	let roundResult,
+		round = 1;
+	while (round <= 5) {
+		roundResult = playRound(playerChoice());
+		console.log(roundResult);
+		if (roundResult.includes('tie')) {
+			continue;
+		}
+		score[roundResult.includes('Win') ? 0 : 1]++;
+		round++;
+	}
+	if (score[0] > score[1]) {
+		console.log(`You win! ${score[0]} to ${score[1]}!`);
+	} else {
+		console.log(`You lose! ${score[1]} to ${score[0]}!`);
+	}
 }
 
 const promptInput = () => prompt("Chose 'Rock', 'Paper', or 'Scissors'");
