6dea4d4abec7af05386fb23576abcf0f1ae02a7c
Chad Kreutzer <kanthalion@gmail.com>
2018-05-30 07:06:08 +0000

    playRound written

--- script.js
+++ script.js
@@ -3,8 +3,28 @@
 const randomNumber = () => Math.floor(Math.random() * 3);
 
 function computerPlay(randomNumber) {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     return choices[randomNumber];
 }
 
 console.log(computerPlay(randomNumber()));
+
+function playRound(playerSelection, computer = computerPlay(randomNumber())) {
+    const player = String(playerSelection).toLowerCase();
+    if (player === "rock" || player === "paper" || player === "scissors") {
+        if (player === computer) {
+            return "It's a tie, try again!";
+        }
+        switch (player) {
+            case "rock":
+                return (computer === 'scissors') ? "You Win! Rock beats Scissors!" : "You Lose! Paper beats Rock!";
+            case "paper":
+                return (computer === 'rock') ? "You Win! Paper beats Rock!" : "You Lose! Scissors beats Paper!";
+            default:
+                return (computer === 'paper') ? "You Win! Scissors beats Paper!" : "You Lose! Rock beats Scissors!";
+        }
+    }
+    throw new Error("You must chose 'Rock', 'Paper', or 'Scissors'");
+}
+
+console.log(playRound(true));
